7d2158f8282f978eb1d705cad4704578c5e35ef1
Mehdi El Mir <midox0044@gmail.com>
2022-05-30 17:49:10 +0100

    menu Admin

--- resources/views/layouts/primary.blade.php
+++ resources/views/layouts/primary.blade.php
@@ -29,7 +29,8 @@
     <div class="page-loader-wrapper">
         <div class="loader">
             <div class="m-t-30">
-                <img src="{{ asset('landing/assets/img/mymed_logo_light.png') }}" width="128" height="64" alt="MyMed">
+                <img src="{{ asset('landing/assets/img/mymed_logo_light.png') }}" width="128" height="64"
+                    alt="MyMed">
             </div>
             <p>Please wait...</p>
         </div>
@@ -42,10 +43,9 @@
                     <button type="button" class="btn-toggle-offcanvas"><i class="lnr lnr-menu fa fa-bars"></i></button>
                 </div>
                 <div class="navbar-brand">
-                    <a
-                        href="{{ Auth::user()->utype == 'ADM' ? route('landing-home') : route('landing-home') }}">
-                        <img src="{{ asset('landing/assets/img/logo_mymed_new.png') }}" width="128" height="40" alt="My MED"
-                            class="img-responsive logo"></a>
+                    <a href="{{ Auth::user()->utype == 'ADM' ? route('landing-home') : route('landing-home') }}">
+                        <img src="{{ asset('landing/assets/img/logo_mymed_new.png') }}" width="128" height="40"
+                            alt="My MED" class="img-responsive logo"></a>
                 </div>
                 <div class="navbar-right">
                     <form id="navbar-search" class="navbar-form search-form">
@@ -228,97 +228,11 @@
                             <ul class="main-menu metismenu">
                                 <li class="active"><a href="{{ route('admin-dashboard') }}"><i
                                             class="icon-home"></i><span>Dashboard</span></a></li>
-                                <li><a href="{{ route('admin-rendez_vous') }}"><i class="icon-calendar"></i>Appointment</a></li>
-                                <li><a href="app-taskboard.html"><i class="icon-list"></i>Taskboard</a></li>
-                                <li><a href="app-inbox.html"><i class="icon-home"></i>Inbox App</a></li>
-                                <li><a href="app-chat.html"><i class="icon-bubbles"></i>Chat App</a></li>
-                                <li><a href="javascript:void(0);" class="has-arrow"><i
-                                            class="icon-user-follow"></i><span>Doctors</span> </a>
-                                    <ul>
-                                        <li><a href="doctors-all.html">All Doctors</a></li>
-                                        <li><a href="doctor-add.html">Add Doctor</a></li>
-                                        <li><a href="doctor-profile.html">Doctor Profile</a></li>
-                                        <li><a href="doctor-events.html">Doctor Schedule</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="javascript:void(0);" class="has-arrow"><i
-                                            class="icon-user"></i><span>Patients</span> </a>
-                                    <ul>
-                                        <li><a href="patients-all.html">All Patients</a></li>
-                                        <li><a href="patient-add.html">Add Patient</a></li>
-                                        <li><a href="patient-profile.html">Patient Profile</a></li>
-                                        <li><a href="patient-invoice.html">Invoice</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="javascript:void(0);" class="has-arrow"><i
-                                            class="icon-wallet"></i><span>Payments</span> </a>
-                                    <ul>
-                                        <li><a href="payments.html">Payments</a></li>
-                                        <li><a href="payments-add.html">Add Payment</a></li>
-                                        <li><a href="payments-invoice.html">Invoice</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="javascript:void(0);" class="has-arrow"><i
-                                            class="icon-layers"></i><span>Departments</span> </a>
-                                    <ul>
-                                        <li><a href="depa-add.html">Add</a></li>
-                                        <li><a href="depa-all.html">All Departments</a></li>
-                                        <li><a href="javascript:void(0);">Cardiology</a></li>
-                                        <li><a href="javascript:void(0);">Pulmonology</a></li>
-                                        <li><a href="javascript:void(0);">Gynecology</a></li>
-                                        <li><a href="javascript:void(0);">Neurology</a></li>
-                                        <li><a href="javascript:void(0);">Urology</a></li>
-                                        <li><a href="javascript:void(0);">Gastrology</a></li>
-                                        <li><a href="javascript:void(0);">Pediatrician</a></li>
-                                        <li><a href="javascript:void(0);">Laboratory</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="our-centres.html"><i class="icon-pointer"></i>WorldWide Centres</a></li>
-                                <li>
-                                    <a href="#Authentication" class="has-arrow"><i
-                                            class="icon-lock"></i><span>Authentication</span></a>
-                                    <ul>
-                                        <li><a href="page-login.html">Login</a></li>
-                                        <li><a href="page-register.html">Register</a></li>
-                                        <li><a href="page-lockscreen.html">Lockscreen</a></li>
-                                        <li><a href="page-forgot-password.html">Forgot Password</a></li>
-                                        <li><a href="page-404.html">Page 404</a></li>
-                                        <li><a href="page-403.html">Page 403</a></li>
-                                        <li><a href="page-500.html">Page 500</a></li>
-                                        <li><a href="page-503.html">Page 503</a></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="#Widgets" class="has-arrow"><i
-                                            class="icon-puzzle"></i><span>Widgets</span></a>
-                                    <ul>
-                                        <li><a href="widgets-statistics.html">Statistics Widgets</a></li>
-                                        <li><a href="widgets-data.html">Data Widgets</a></li>
-                                        <li><a href="widgets-chart.html">Chart Widgets</a></li>
-                                        <li><a href="widgets-weather.html">Weather Widgets</a></li>
-                                        <li><a href="widgets-social.html">Social Widgets</a></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="#Pages" class="has-arrow"><i class="icon-docs"></i><span>Extra
-                                            Pages</span></a>
-                                    <ul>
-                                        <li><a href="page-blank.html">Blank Page</a> </li>
-                                        <li><a href="doctor-profile.html">Profile</a></li>
-                                        <li><a href="page-gallery.html">Image Gallery <span
-                                                    class="badge badge-default float-right">v1</span></a> </li>
-                                        <li><a href="page-gallery2.html">Image Gallery <span
-                                                    class="badge badge-warning float-right">v2</span></a> </li>
-                                        <li><a href="page-timeline.html">Timeline</a></li>
-                                        <li><a href="page-timeline-h.html">Horizontal Timeline</a></li>
-                                        <li><a href="page-pricing.html">Pricing</a></li>
-                                        <li><a href="page-search-results.html">Search Results</a></li>
-                                        <li><a href="page-helper-class.html">Helper Classes</a></li>
-                                        <li><a href="page-maintenance.html">Maintenance</a></li>
-                                        <li><a href="page-testimonials.html">Testimonials</a></li>
-                                        <li><a href="page-faq.html">FAQs</a></li>
-                                    </ul>
-                                </li>
+                                <li><a href="app-taskboard.html"><i class="icon-users"></i>Users</a></li>
+                                <li><a href="{{ route('admin-rendez_vous') }}"><i
+                                            class="icon-calendar"></i>Appointment</a></li>
+                                <li><a href="app-inbox.html"><i class="icon-bubbles"></i>Contacts</a></li>
+                                <li><a href="app-inbox.html"><i class="icon-puzzle"></i>Blog</a></li>
                             </ul>
                         </nav>
                     </div>
