3dc58a9929d39b69ba8a3b0893cb6c5198d3106f
unknown <midox0044@gmail.com>
2022-06-06 12:05:43 +0100

    user show

--- routes/web.php
+++ routes/web.php
@@ -79,6 +79,10 @@ Route::middleware('auth','auth:sanctum','adminauth')->group(function(){
 
 });
 
+//comment
+Route::get('/hello',function(){
+    return ['hello'=>'world'];
+});
 
 //Route users
 Route::middleware('auth','auth:sanctum')->group(function(){
