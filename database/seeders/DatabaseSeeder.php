ff1389d5e9eb3f7cf778a7d78d11107ad0233c17
Otmane AMAL <104436603+otmaneamaldev90@users.noreply.github.com>
2022-06-07 15:31:40 +0100

    otmane amal

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,62 +18,57 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
+
+        $name = 'admin admin';
+        $slug = Str::slug($name);
+
         DB::table('users')->insert(
             [
-<<<<<<< HEAD
-                'name'=>'admin',
-                'email'=>'admin@mymed.ma',
-                'phone'=>'0625223212',
-                'password'=> Hash::make('123456789'),
-                'utype' => 'ADM',
-                'slug' => 'ADM'
-=======
                 'name' => 'admin',
                 'email' => 'admin@mymed.ma',
                 'phone' => '0625223212',
                 'password' => Hash::make('123456789'),
-                'utype' => 'ADM'
->>>>>>> 68568974a63a528e78e4420caabb24d0c493a71e
+                'utype' => 'ADM',
+                'slug' => $slug,
             ]
         );
 
-<<<<<<< HEAD
-            DB::table('users')->insert(
-                [
-                    'name'=>'user',
-                    'email'=>'user@mymed.ma',
-                    'phone'=>'0632652212',
-                    'password'=> Hash::make('123456789'),
-                    'utype' => 'USR',
-                    'slug' => 'USR'
-                ]
-                );
-=======
+
+        $name = 'user user';
+        $slug = Str::slug($name);
+
         DB::table('users')->insert(
             [
                 'name' => 'user',
                 'email' => 'user@mymed.ma',
                 'phone' => '0632652212',
                 'password' => Hash::make('123456789'),
-                'utype' => 'USR'
+                'utype' => 'USR',
+                'slug' => $slug,
             ]
         );
->>>>>>> 68568974a63a528e78e4420caabb24d0c493a71e
 
-        DB::table('type_cabinets')->insert(
-            [
-                'name' => 'Cabinet Dentist',
-            ]
-        );
-        DB::table('type_cabinets')->insert(
-            [
-                'name' => 'Cabinet Doctor general',
-            ]
-        );
-        DB::table('type_cabinets')->insert(
-            [
-                'name' => 'Cabinet Doctor specialist',
-            ]
-        );
+        // $name = 'Cabinet Dentist';
+        // $slug = Str::slug($name);
+        // DB::table('type_cabinets')->insert(
+        //     [
+        //         'name' => 'Cabinet Dentist',
+        //         'slug' =>$slug,
+        //     ]
+        // );
+
+        // $name = 'Cabinet Doctor general';
+        // $slug = Str::slug($name);
+
+        // DB::table('type_cabinets')->insert(
+        //     [
+        //         'name' => 'Cabinet Doctor general',
+        //     ]
+        // );
+        // DB::table('type_cabinets')->insert(
+        //     [
+        //         'name' => 'Cabinet Doctor specialist',
+        //     ]
+        // );
     }
 }
